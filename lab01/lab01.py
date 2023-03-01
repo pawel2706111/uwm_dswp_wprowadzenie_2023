039141daa014409c7806f1957239d5b4e93c7bc9
pawel2706111 <59094564+pawel2706111@users.noreply.github.com>
2023-03-01 15:54:17 +0100

    update lab01

--- lab01/lab01.py
+++ lab01/lab01.py
@@ -7,13 +7,13 @@
 print('zadanie 1')
 int_1 = 10
 int_2 = int('47', base=8)
-print('\tint 1:', int_1)
-print('\tint 2:', int_2)
+print('int 1:', int_1)
+print('int 2:', int_2)
 
 float_1 = 0.2 + 0.1
 float_2 = float('0.357')
-print('\tfloat 1:', float_1)
-print('\tfloat 2:', float_2)
+print('float 1:', float_1)
+print('float 2:', float_2)
 
 # zadanie 2
 print('\nzadanie 2')
@@ -29,9 +29,7 @@ for i in range(0, 3):
 
 # float.is_integer()
 print('\nWykorzystanie float.is_integer()')
-
 floaty = [5.56, 4.7, 3.0]
-
 for i in floaty:
     print('Liczba', i, end=' ')
     if(i.is_integer()):
@@ -39,5 +37,16 @@ for i in floaty:
     else:
         print('NIE jest liczbą całkowitą')
 
-
 # zadanie 3
+
+liczba = 0b100010
+print('\nzadanie 3')
+print(liczba, '-', bin(liczba))
+print('najmłodszy bit liczby', int(liczba), 'to', liczba & 1)
+print('najstarszy bit liczby', int(liczba), 'to', liczba >> (liczba.bit_length()-1))
+print('2 ** 5 =', (1 << 5))
+print(' p | q | p ^ q')
+print(' 0 | 0 |  ', 0 ^ 0)
+print(' 0 | 1 |  ', 0 ^ 1)
+print(' 1 | 0 |  ', 1 ^ 0)
+print(' 1 | 1 |  ', 1 ^ 1)
