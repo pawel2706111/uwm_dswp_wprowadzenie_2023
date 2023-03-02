1672ca9f3ac21bf4b6635bbd815cf8b7b073eb6b
pawel2706111 <59094564+pawel2706111@users.noreply.github.com>
2023-03-02 18:07:40 +0100

    Update lab02

--- lab02/lab02_zad_4.py
+++ lab02/lab02_zad_4.py
@@ -3,3 +3,18 @@
 # 155090
 # Grupa 2
 # lab 02
+#
+# zadanie 4
+
+# dodawanie pod kreską
+
+
+liczba_1 = '234'
+liczba_2 = '135'
+kreska = '+ ' + '-' * len(liczba_1)
+
+nowy_format = '{:>'+str(len(kreska))+'}'
+print(nowy_format.format(liczba_1))
+print(nowy_format.format(liczba_2))
+print(kreska)
+print(nowy_format.format(str(int(liczba_1)+int(liczba_2))))
