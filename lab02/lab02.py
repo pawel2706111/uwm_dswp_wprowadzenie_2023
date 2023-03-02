af011f0074e9ebb2e85e4de6366313737a643b75
pawel2706111 <59094564+pawel2706111@users.noreply.github.com>
2023-03-02 17:26:11 +0100

    Update lab02

--- lab02/lab02.py
+++ lab02/lab02.py
@@ -23,3 +23,16 @@ print(nowe_dane)
 
 # zadanie 2
 lipsum = 'Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker'
+
+# zadanie 3
+
+print('\nZadanie 3')
+imie = "Paweł"
+nazwisko = "Majorowski"
+litera_1 = imie[2]
+litera_2 = nazwisko[3]
+
+liczba_liter1 = lipsum.count(litera_1)
+liczba_liter2 = lipsum.count(litera_2)
+
+print(f'W tekście jest {liczba_liter1} liter "{litera_1}" oraz {liczba_liter2} liter "{litera_2}".')
