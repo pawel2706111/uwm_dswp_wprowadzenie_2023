0bf1d88a59e812659018dfabfba66e7f3a69f45c
pawel2706111 <59094564+pawel2706111@users.noreply.github.com>
2023-03-07 00:25:23 +0100

    Update lab02

--- lab02/lab02_zad_4.py
+++ lab02/lab02_zad_4.py
@@ -6,15 +6,32 @@
 #
 # zadanie 4
 
-# dodawanie pod kreską
+# Wycięcie z napisu pierwszych pięciu liter
+print('wycięcie z napisu abcdefg trzech pierwszych liter:')
+print(f'{"abcdefg":.3}')
 
+# Znak liczby (domyślnie znak + nie jest wyświetlany, tylko - jest wyświetlany)
+print('\nwyświetlanie znaku liczby:')
+liczba = 5
+print(f'{liczba:+d}')
+print(f'{-liczba:+d}')
 
-liczba_1 = '234'
-liczba_2 = '135'
-kreska = '+ ' + '-' * len(liczba_1)
+# wyrównanie do środka
+print('\nwyrówanie do środka:')
+print('+' + '-' * 18 + '+')
+print(f'+{"nazwa kolumny":^18}+')
+print('+' + '-' * 18 + '+')
 
-nowy_format = '{:>'+str(len(kreska))+'}'
-print(nowy_format.format(liczba_1))
-print(nowy_format.format(liczba_2))
-print(kreska)
-print(nowy_format.format(str(int(liczba_1)+int(liczba_2))))
+# formatowanie daty
+from datetime import datetime
+
+print('\nformatowanie daty:')
+print(f'teraz: {datetime.now():%d %m %y %H:%M}')
+
+# użycie słownika
+
+auto = {'marka': 'Mercedes',
+        'model': 'SLS'}
+
+print('\nUżycie słownika:')
+print(f'{auto["marka"]} {auto["model"]}')
